cf4917e38c30a6cf069ceb9e33366ed79490c65b
lmcuong <lmcuongcv@gmail.com>
2021-10-18 19:45:18 +0700

    Gan xong hieu ung

---
+++ src/data/quiz.js
@@ -0,0 +1,180 @@
+var quizs = [
+  /* Câu số
+    ========================================================================== */
+  {
+    id: 0,
+    messages: [
+      
+      {
+        type: "emoji",
+        value: "&#128516;",
+        duration: 1500,
+      },
+      {
+        type: "image",
+        value: "",
+        url: "./assets/image/anhdaidien.jpg",
+        alt: "Anh Tùng Anh",
+        duration: 1500,
+      },
+      
+      {
+        type: "question",
+        value: "Cậu ở đâu đến đây vậy?",
+        duration: 1500,
+      },
+      {
+        type: "text",
+        value: "Chào mừng câu đến với nhóm chat của Boy BeONE :>",
+        duration: 1500,
+      },
+      // {
+      //     type: "keyend",
+      //     value: "Bạn có ở trong CLB BeONE không?",
+      //     duration: 1500,
+      // },
+    ],
+    answers: ["Tớ ở BeONE <3", "Tớ ở một nơi khác :))"],
+    afterAnswers: [
+      [
+        {
+          type: "emoji",
+          value: "&#128516;",
+          duration: 1500,
+        },
+        {
+          type: "image",
+          value: "",
+          url: "./assets/image/anhdaidien.jpg",
+          alt: "Anh Tùng Anh",
+          duration: 1500,
+        },
+        
+      ],
+    ],
+  },
+  /* 
+    ========================================================================== */
+
+  {
+    id: 0,
+    messages: [
+      {
+        type: "text",
+        value: "Chào mừng câu đến với nhóm chat của Boy BeONE :>",
+        duration: 1500,
+      },
+      {
+        type: "emoji",
+        value: "&#128516;",
+        duration: 1500,
+      },
+      {
+        type: "keyend",
+        value: '<div style="font-weight: bold; font-size: 1.4rem;">The Earth is Happy <br>WHEN WOMEN SMILE</div><br><div style="font-family: Ephesis, cursive; font-size: 1.7rem;"> Phái nữ một nửa của thế giới luôn quan trọng với phát nam. Chúc cho những điều may mắn, hạnh phúc sẽ tới với cậu!</div>',
+        duration: 1500,
+      },
+
+      {
+        type: "question",
+        value: "Theo cậu, ai là Nam thần BeONE?",
+        duration: 1500,
+      },
+      // {
+      //     type: "keyend",
+      //     value: "Bạn có ở trong CLB BeONE không?",
+      //     duration: 1500,
+      // },
+    ],
+    answers: ["Anh Bảo", "Anh Tùng Anh", "Lê Văn Đức", "Đăng Hoàng Hải"],
+    afterAnswers: [
+      [
+        {
+          type: "text",
+          value: "Anh Bảo chưa có ng iu đâu cậu",
+          duration: 1500,
+				},
+				{
+          type: "text",
+          value: "Quá chuẩn luôn ",
+          duration: 1500,
+				},
+				{
+          type: "text",
+          value: "Đức chưa có ng iu đâu cậu",
+          duration: 1500,
+        },
+
+        {
+          type: "text",
+          value: "Hải chưa có ng iu đâu cậu",
+          duration: 1500,
+        },
+      ],
+    ],
+  },
+  /* 
+    ========================================================================== */
+
+  {
+    id: 0,
+    messages: [
+      {
+        type: "text",
+        value: "Người anh Tâm lý luôn quan tâm đến các em!",
+        duration: 1500,
+			},
+			{
+        type: "text",
+        value: "Thực ra anh Tùng Anh chưa có người thương đâu cậu",
+        duration: 1500,
+			},
+			{
+        type: "emoji",
+        value: "&#128522;",
+        duration: 1500,
+      },
+      
+      
+
+      {
+        type: "question",
+        value: "Theo cậu, ai là Nam thần BeONE?",
+        duration: 1500,
+      },
+      // {
+      //     type: "keyend",
+      //     value: "Bạn có ở trong CLB BeONE không?",
+      //     duration: 1500,
+      // },
+    ],
+    answers: ["Anh Bảo", "Anh Tùng Anh", "Lê Văn Đức", "Đăng Hoàng Hải"],
+    afterAnswers: [
+      [
+        {
+          type: "emoji",
+          value: "&#128516;",
+          duration: 1500,
+        },
+        {
+          type: "image",
+          value: "",
+          url: "./assets/image/anhdaidien.jpg",
+          alt: "Anh Tùng Anh",
+          duration: 1500,
+        },
+        {
+          type: "text",
+          value: "Xin chào",
+          duration: 1500,
+        },
+
+        {
+          type: "question",
+          value: "Theo cậu, ai là Nam thần BeONE?",
+          duration: 1500,
+        },
+      ],
+    ],
+  },
+];
